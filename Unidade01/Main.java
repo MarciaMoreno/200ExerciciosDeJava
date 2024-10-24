eea2aa5ce8c63d32399e568202c4fc85c89c2db1
Marcia P. Moreno <mapat.soad@gmail.com>
2024-10-24 18:45:41 -0300

    Inserção da Classe Main.java

---
+++ Unidade01/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Exibindo Olá mundo no console...
+        System.out.println("Olá Mundo... Esta é minha primeira classe em Java!..");
+        // Modificado para exibir o nome completo...
+        System.out.println("Meu nome é Marcia Patricia Moreno...");
+    }
+}
